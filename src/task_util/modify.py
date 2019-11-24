fcfdcfafecdaca75dec58fde46121e5dfcf1a7d6
godfrey-leung <49004276+godfrey-leung@users.noreply.github.com>
2019-11-24 23:55:11 +0000

    Corrected the input line code

--- src/task_util/modify.py
+++ src/task_util/modify.py
@@ -27,9 +27,10 @@ def modify(task_list):
         task_index_to_modify = name_check.index(True)
 
         # Ask user the attribute to be modified
-        attr = input("Please choose the attribute to be modified: ", \
-                     "(1: start_date; 2: deadline; 3: completed; ", \
-                     "anything else: quit)")
+        ask_attr_str = "Please choose the attribute to be modified: " +
+                     "(1: start_date; 2: deadline; 3: completed; " +
+                     "anything else: quit)"
+        attr = input(ask_attr_str)
 
         if attr == "1":
             new_value = input("Please enter the new start_date: ")
